64116e24358e8e6303da41f046cfdb93cacda0d0
Ilya Pavlov <iluxz@mail.ru>
2018-01-17 23:34:06 +0300

    ic

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Win32::File::Next - It's new $module
+
+# SYNOPSIS
+
+    use Win32::File::Next;
+
+# DESCRIPTION
+
+Win32::File::Next is ...
+
+# LICENSE
+
+Copyright (C) Ilya Pavlov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Ilya Pavlov <iluxz@mail.ru>
---
+++ lib/Win32/File/Next.pm
@@ -0,0 +1,41 @@
+package Win32::File::Next;
+
+use strict;
+use warnings;
+
+our $VERSION = "0.001000";
+
+
+
+1;
+
+
+
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Win32::File::Next - It's new $module
+
+=head1 SYNOPSIS
+
+    use Win32::File::Next;
+
+=head1 DESCRIPTION
+
+Win32::File::Next is ...
+
+=head1 LICENSE
+
+Copyright (C) Ilya Pavlov.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Ilya Pavlov E<lt>ilux@cpan.orgE<gt>
+
+=cut
